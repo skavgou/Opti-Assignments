06e175c77b7605c696981db6c69ae09c9dd1d557
Kuriboh-Lord <106563396+Kuriboh-Lord@users.noreply.github.com>
2026-02-08 20:09:54 +0000

    q1 code

--- A1.py
+++ A1.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error
 from sympy.physics.paulialgebra import delta
+import decimal
+decimal.getcontext().prec = 100
 
 x = sp.symbols('x')
 fx = x**4
@@ -42,13 +44,15 @@ plt.show()
 
 class myFun():
     def f(self, x):
-        return x**4 # function value f(x)
+        return float(decimal.Decimal(x)**4) # function value f(x)
     def df(self, x):
-        return 4*x**3 # derivative of f(x)
+        return float(4*(decimal.Decimal(x))**3) # derivative of f(x)
 
 my_fun = myFun()
 
-def gradDescent(fn,x0,alpha=0.15,num_iters=50):
+num_iters = 50
+
+def gradDescent(fn,num_iters,x0,alpha):
     x=x0;
     X=np.array([x]); F=np.array(fn.f(x));
     for k in range(num_iters):
@@ -57,7 +61,20 @@ def gradDescent(fn,x0,alpha=0.15,num_iters=50):
         X=np.append(X,[x],axis=0); F=np.append(F,fn.f(x))
     return (X,F)
 
-(X1, F1) = gradDescent(my_fun,x0=1,alpha=0.05)
-(X2, F2) = gradDescent(my_fun,x0=1,alpha=0.5)
-(X3, F3) = gradDescent(my_fun,x0=1,alpha=1.2)
+(X1, F1) = gradDescent(my_fun,num_iters,x0=1,alpha=0.05)
+(X2, F2) = gradDescent(my_fun,num_iters,x0=1,alpha=0.5)
+(X3, F3) = gradDescent(my_fun,num_iters,x0=1,alpha=1.2)
+
+num_iters+=2
+plt.plot(list(range(1, num_iters)), X1, color = 'blue', label='Aplha = 0.05')
+plt.plot(list(range(1, num_iters)), X2, color = 'green', label='Aplha = 0.5')
+#plt.plot(list(range(1, num_iters)), X3, color = 'red', label='Aplha = 1.2')
+plt.legend()
+plt.show()
+
+plt.plot(list(range(1, num_iters)), F1, color = 'blue', label='Aplha = 0.05')
+plt.plot(list(range(1, num_iters)), F2, color = 'green', label='Aplha = 0.5')
+#plt.plot(list(range(1, num_iters)), F3, color = 'red', label='Aplha = 1.2')
+plt.legend()
+plt.show()
 
